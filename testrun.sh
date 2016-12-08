9eca405fb446f423efb3c41d677571c52a090057
michael-conway <michael_conway@unc.edu>
2016-12-08 08:56:43 -0500

    added test run statement

---
+++ testrun.sh
@@ -0,0 +1,1 @@
+docker run --rm -a stdout -a stderr -e testde2 -e testde2 --log-driver=none -v /home/mconway/temp/hydrodata:/de-app-work  --name hydro -w /de-app-work --name hydro --net=bridge  --entrypoint="/PopulationVsSoilMoisture.scr" diceunc/hydro1:1.0
